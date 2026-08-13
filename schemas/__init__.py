738b429a3afd02da8d1179db93111d51070ec30c
fatimaarzoo <arzoo14100@gmail.com>
2026-08-13 15:16:40 +0530

    Update __init__.py

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,6 +1,7 @@
 from schemas.payload.course import CourseAdd, CourseUpdate
 from schemas.payload.student import StudentAdd, StudentUpdate, SelectCourse
 from schemas.payload.teacher import TeacherAdd
+from schemas.payload.accounts import *
 
 from schemas.response.course import CourseResponse
 from schemas.response.student import StudentResponse
